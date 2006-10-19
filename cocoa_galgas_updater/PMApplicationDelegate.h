2c12a1b9465f55904b699d8d9358b507fb5264de
Pierre Molinaro <pierre@pcmolinaro.name>
2006-10-19 07:31:33 +0000

    suite maj cocoa galgas updater

--- cocoa_galgas_updater/PMApplicationDelegate.h
+++ cocoa_galgas_updater/PMApplicationDelegate.h
@@ -7,12 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Security/Security.h>
 
 
 @interface PMApplicationDelegate : NSObject {
   @protected IBOutlet NSTextField * mStatusTextField ;
   @protected NSTimer * mTimer ;
   @protected NSString * mCurrentCocoaGalgasApplicationPath ;
+  
+  @protected IBOutlet NSView * mAccessoryViewForOpenPanel ;
+  @protected NSOpenPanel * mLIBPMOpenPanel ;
+  @protected AuthorizationRef mAuthorizationRef ;
 }
+
+- (IBAction) newFolderAction: (id) inSender ;
 
 @end
