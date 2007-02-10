edbc62cd4134bf1eee9a40ce972054d8459d9530
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2007-02-10 14:29:41 +0000

    suite implementation domaine

--- hand_coded_sources/domain_generation.cpp
+++ hand_coded_sources/domain_generation.cpp
@@ -20,6 +20,7 @@
 //---------------------------------------------------------------------------*
 
 #include "semantics_semantics.h"
+#include "utilities/MF_MemoryControl.h"
 
 //---------------------------------------------------------------------------*
 
@@ -45,8 +46,27 @@ generateHdeclarations (AC_OutputStream & inHfile,
   inHfile.writeCppTitleComment (C_String ("Class for '") + mDomainName + "' Domain") ;
   
   inHfile << "class GGS_" << mDomainName << " : public AC_galgas_domain {\n" ;
-  
-  inHfile << "}\n\n" ;
+  GGS_domainAttributeMap::element_type * currentAttribute = mAttributeMap.firstObject () ;
+  while (currentAttribute != NULL) {
+    macroValidPointer (currentAttribute) ;
+    inHfile << "//--- '" << currentAttribute->mKey << "' attribute\n"
+               "  protected : cDomain _attribute_" << currentAttribute->mKey << " ;\n"
+               "  public : void modifier_addTo"
+            << currentAttribute->mKey.stringWithUpperCaseFirstLetter ()
+            << " (C_Lexique & inLexique,\n"
+               "                                    const GGS_string & inNewValue\n"
+               "                                    COMMA_LOCATION_ARGS) ;\n\n" ;
+    currentAttribute = currentAttribute->nextObject () ;
+  }
+  inHfile << "//--- Constructor\n"
+             "  public : GGS_" << mDomainName << " (void) ;\n\n"
+             "//--- Destructor\n"
+             "  public : virtual ~GGS_" << mDomainName << " (void) ;\n\n"
+             "//--- 'emptyDomain' GALGAS constructor\n"
+             "  public : static GGS_" << mDomainName << "\n"
+             "  constructor_emptyDomain (C_Lexique & inLexique\n"
+             "                           COMMA_LOCATION_ARGS) ;\n"
+             "} ;\n\n" ;
 }
 
 //---------------------------------------------------------------------------*
@@ -73,6 +93,42 @@ generateCppClassImplementation (AC_OutputStream & inCppFile,
                                   sint32 & /* ioPrototypeIndex */,
                                   const bool /* inGenerateDebug */) const {
   inCppFile.writeCppTitleComment (C_String ("Class for '") + mDomainName + "' Domain") ;
+
+//--- Constructor
+  inCppFile << "GGS_" << mDomainName << "::GGS_" << mDomainName << " (void) {\n"
+               "}\n\n" ;
+
+//--- Destructor
+  inCppFile.writeCppHyphenLineComment () ;
+  inCppFile << "GGS_" << mDomainName << "::~GGS_" << mDomainName << " (void) {\n"
+               "}\n\n" ;
+
+//--- 'emptyDomain' constructor
+  inCppFile.writeCppHyphenLineComment () ;
+  inCppFile << "GGS_" << mDomainName << " GGS_" << mDomainName << "::\n"
+               "constructor_emptyDomain (C_Lexique & /* inLexique */\n"
+               "                         COMMA_UNUSED_LOCATION_ARGS) {\n"
+               "  GGS_" << mDomainName << " result ;\n"
+               "  result.mIsBuilt = true ;\n"
+               "  return result ;\n"
+               "}\n\n" ;
+
+//--- Accessors
+  GGS_domainAttributeMap::element_type * currentAttribute = mAttributeMap.firstObject () ;
+  while (currentAttribute != NULL) {
+    macroValidPointer (currentAttribute) ;
+    inCppFile.writeCppHyphenLineComment () ;
+    inCppFile << "void GGS_" << mDomainName << "::\n"
+                 "modifier_addTo"
+            << currentAttribute->mKey.stringWithUpperCaseFirstLetter ()
+            << " (C_Lexique & /* inLexique */,\n"
+               "                                    const GGS_string & inNewValue\n"
+               "                                    COMMA_UNUSED_LOCATION_ARGS) {\n"
+               "  findOrAddEntry (_attribute_" << currentAttribute->mKey
+            << ", inNewValue) ;\n"
+               "}\n\n" ;
+    currentAttribute = currentAttribute->nextObject () ;
+  }
 }
 
 //---------------------------------------------------------------------------*
