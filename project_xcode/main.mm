9f229337e93c3dc403bb07f2bc40666cf708c676
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2007-03-07 22:27:31 +0000

    acceleration cocoa galgas + 2 vues pour le texte

--- project_xcode/main.mm
+++ project_xcode/main.mm
@@ -11,53 +11,7 @@
 
 //---------------------------------------------------------------------------*
 
-@interface PMTextStorage : NSTextStorage {
-
-}
-
-@end
-
-//---------------------------------------------------------------------------*
-
-@implementation PMTextStorage
-
-- (id) init {
-  self = [super init] ;
-  if (self) {
-    NSLog (@"PMTextStorage <init>") ;
-	// Register for "text changed" notifications of our text storage:
-	  [[NSNotificationCenter defaultCenter]
-      addObserver:self
-      selector:@selector(myProcessEditing:)
-      name: NSTextStorageWillProcessEditingNotification
-      object:self
-    ];
-  }
-  return self ;
-}
-
--(void) myProcessEditing: (NSNotification*)inNotification {
-  const NSRange	range = [self editedRange];
-  NSLog (@"Edited Range [%d, %d]", range.location, range.length) ; 
-  unsigned i ;
-  for (i=0 ; i<range.length ; i++) {
-    const NSRange r = {range.location + i, 1} ;
-    [self removeAttribute:NSForegroundColorAttributeName range:r] ;
-    [self
-      addAttribute:NSForegroundColorAttributeName
-      value:((r.location & 1) == 0) ? [NSColor redColor] : [NSColor blueColor]
-      range:r
-    ] ;
-  }
-}
-
-
-@end
-
-//---------------------------------------------------------------------------*
-
 int main (int argc, const char * argv []) {
-//  [PMTextStorage poseAsClass:[NSTextStorage class]] ;
   return NSApplicationMain (argc, argv) ;
 }
 
