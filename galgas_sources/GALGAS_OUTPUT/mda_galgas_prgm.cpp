a53e8aa70df2ae77ba35e2dfb120491712d4a862
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2010-01-28 17:33:26 +0000

    Modif C_String

--- galgas_sources/GALGAS_OUTPUT/mda_galgas_prgm.cpp
+++ galgas_sources/GALGAS_OUTPUT/mda_galgas_prgm.cpp
@@ -919,7 +919,6 @@ int mainForLIBPM  (const int argc, const char * argv []) {
   C_PrologueEpilogue::runEpilogueActions () ;
   C_Object::garbage () ;
   C_StringCommandLineOption::releaseStrings () ;
-  C_String::freeUnusedRegisteredStrings () ;
   #ifndef DO_NOT_GENERATE_CHECKINGS
     C_Object::checkAllObjectsHaveBeenReleased () ;
   #endif
