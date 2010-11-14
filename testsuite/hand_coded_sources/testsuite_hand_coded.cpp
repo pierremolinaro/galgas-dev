15e97fc554feb03f33d484855db39b77bc7d5102
Pierre Molinaro <pierre@pcmolinaro.name>
2010-11-14 15:46:58 +0000

    Mise à jour test et exemples

--- testsuite/hand_coded_sources/testsuite_hand_coded.cpp
+++ testsuite/hand_coded_sources/testsuite_hand_coded.cpp
@@ -1,57 +1,54 @@
 
 #include "testsuite_list.h"
-#include "galgas/GGS_stringset.h"
-#include "galgas/GGS_uint.h"
+#include "testsuite_functions.h"
 #include "galgas2/predefined-types.h"
 
 //---------------------------------------------------------------------------*
 
-void
-routine_test_dictionary (C_Compiler & inCompiler) {
-  const PMSInt32 TEST_SIZE = 1000000 ;
-  GGS_stringset dictionary (GGS_stringset::constructor_emptySet ()) ;
+static void routine_test_dictionary (void) {
+  const PMSInt32 TEST_SIZE = 1000 ;
+  GALGAS_stringset dictionary = GALGAS_stringset::constructor_emptySet (HERE) ;
 //--- First insertion
   for (PMSInt32 i=0 ; i<TEST_SIZE ; i++) {
     C_String s ; s << cStringWithSigned (i) ;
-    dictionary.addAssign_operation (GGS_string (true, s)) ;
+    dictionary.addAssign_operation (GALGAS_string (s) COMMA_HERE) ;
   }
   printf ("Insertion Test Done\n") ;
 //--- First remove
   for (PMSInt32 i=0 ; i<TEST_SIZE ; i++) {
     C_String s ; s << cStringWithSigned (i) ;
-    dictionary.modifier_removeKey (inCompiler, GGS_string (true, s) COMMA_HERE) ;
+    dictionary.modifier_removeKey (GALGAS_string (s) COMMA_HERE) ;
   }
   printf ("Delete Test Done\n") ;
 //--- Second insertion
   for (PMSInt32 i=0 ; i<TEST_SIZE ; i++) {
     C_String s ; s << cStringWithSigned (i) ;
-    dictionary.addAssign_operation (GGS_string (true, s)) ;
+    dictionary.addAssign_operation (GALGAS_string (s) COMMA_HERE) ;
   }
   printf ("Insertion Test Done\n") ;
 //--- Second Remove
   for (PMSInt32 i=0 ; i<TEST_SIZE ; i++) {
     C_String s ; s << cStringWithSigned (i) ;
-    dictionary.modifier_removeKey (inCompiler, GGS_string (true, s) COMMA_HERE) ;
+    dictionary.modifier_removeKey (GALGAS_string (s) COMMA_HERE) ;
   }
   printf ("Delete Test Done\n") ;
 }
 
 //---------------------------------------------------------------------------*
 
-void
-routine_testsuite_hand_coded (C_Compiler & /* inLexique */
-                              COMMA_UNUSED_LOCATION_ARGS) {
-//  routine_test_dictionary () ;
+void routine_testsuite_5F_hand_5F_coded (C_Compiler * /* inCompiler */
+                                         COMMA_UNUSED_LOCATION_ARGS) {
+  routine_test_dictionary () ;
 }
 
 //---------------------------------------------------------------------------*
 
-GGS_uint
-function_externTestFunction (C_Compiler & _inLexique,
-                             GGS_uint inArg1,
-                             const GGS_uint inArg2
+GALGAS_uint
+function_externTestFunction (const GALGAS_uint & inArg1,
+                             const GALGAS_uint & inArg2,
+                             C_Compiler * inCompiler
                              COMMA_LOCATION_ARGS) {
-  return inArg1.add_operation (_inLexique, inArg2 COMMA_THERE) ;
+  return inArg1.add_operation (inArg2, inCompiler COMMA_THERE) ;
 }
 
 //---------------------------------------------------------------------------*
