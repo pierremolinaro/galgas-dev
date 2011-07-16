6c1daf1fd4800ba0b73000f687d2f4450b7903f8
Pierre Molinaro <pierre@pcmolinaro.name>
2011-07-16 13:17:35 +0000

    Correction bug comparaison instances de classes.

--- libpm/galgas2/acPtr_class.cpp
+++ libpm/galgas2/acPtr_class.cpp
@@ -4,7 +4,7 @@
 //                                                                           *
 //  This file is part of libpm library                                       *
 //                                                                           *
-//  Copyright (C) 2008, ..., 2010 Pierre Molinaro.                           *
+//  Copyright (C) 2008, ..., 2011 Pierre Molinaro.                           *
 //                                                                           *
 //  e-mail : molinaro@irccyn.ec-nantes.fr                                    *
 //                                                                           *
