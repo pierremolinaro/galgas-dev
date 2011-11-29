a304fb5eb2ca5d663d763bcbba8ebc90a3a73bd6
Pierre Molinaro <pierre@pcmolinaro.name>
2011-11-29 22:40:34 +0000

    1re tentative modif document

---
+++ libpm/cocoa_galgas/OC_GGS_DocumentInterface.h
@@ -0,0 +1,112 @@
+//---------------------------------------------------------------------------*
+//                                                                           *
+//  This file is part of libpm library                                       *
+//                                                                           *
+//  Copyright (C) 2003, ..., 2010 Pierre Molinaro.                           *
+//                                                                           *
+//  e-mail : molinaro@irccyn.ec-nantes.fr                                    *
+//                                                                           *
+//  IRCCyN, Institut de Recherche en Communications et Cybernetique de Nantes*
+//  ECN, Ecole Centrale de Nantes (France)                                   *
+//                                                                           *
+//  This library is free software; you can redistribute it and/or modify it  *
+//  under the terms of the GNU Lesser General Public License as published    *
+//  by the Free Software Foundation; either version 2 of the License, or     *
+//  (at your option) any later version.                                      *
+//                                                                           *
+//  This program is distributed in the hope it will be useful, but WITHOUT   *
+//  ANY WARRANTY; without even the implied warranty of MERCHANDIBILITY or    *
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for *
+//  more details.                                                            *
+//                                                                           *
+//---------------------------------------------------------------------------*
+
+#import <Cocoa/Cocoa.h>
+
+//---------------------------------------------------------------------------*
+
+@class OC_GGS_TextView ;
+@class OC_GGS_DelegateForSyntaxColoring ;
+@class OC_Lexique ;
+@class OC_GGS_RulerViewForCompileMessageView ;
+@class OC_GGS_ErrorOrWarningDescriptor ;
+@class OC_GGS_TextSyntaxColoring ;
+@class PMTabBarView ;
+@class OC_GGS_SourceScrollView ;
+@class OC_GGS_Document ;
+
+//---------------------------------------------------------------------------*
+
+@interface OC_GGS_DocumentInterface : NSObject <NSTextViewDelegate, NSWindowDelegate> {
+  @private OC_GGS_Document * mDocument ;
+  
+  @private IBOutlet NSWindow * mWindow ;
+
+  @private IBOutlet NSTextView * mIssueTextView ;
+
+  @private IBOutlet NSSplitView * mIssueSplitView ;
+
+
+  @private IBOutlet NSView * mSourceHostView ;
+
+  @private IBOutlet NSPopUpButton * mEntryListPopUpButton ;
+  
+  @private IBOutlet NSButton * mCurrentLineButton ;
+
+  @private NSTask * mTask ;
+
+  @private NSMutableData * mBufferedInputData ;
+
+  @private NSArrayController * mIssueArrayController ;  // Of PMIssueDescriptor
+  @private IBOutlet NSTableView * mIssueTableView ;
+  @private IBOutlet NSTableColumn * mIssueTableViewColumn ;
+
+  @private IBOutlet NSPanel * mUpdateFromFileSystemPanel ;
+
+  @private IBOutlet NSTextField * mSourceEncodingTextField ;
+  @private NSStringEncoding mFileEncoding ;
+
+//---
+  @private NSArrayController * mSourceDisplayArrayController ;
+
+//---
+  @private IBOutlet PMTabBarView * mTabBarView ;
+
+//--- "Goto Line" sheet
+  @private IBOutlet NSWindow * mGotoWindow ;
+  @private IBOutlet NSTextField * mGotoLineTextField ;
+
+//--- Detailled issue message
+  @private IBOutlet NSTextView * mDetailedIssueTextView ;
+  @private IBOutlet NSSplitView * mDetailedIssueSplitView ;
+
+//--- Build, stop button
+  @private IBOutlet NSButton * mStartBuildButton ;
+  @private IBOutlet NSProgressIndicator * mBuildProgressIndicator ;
+  @private IBOutlet NSButton * mStopBuildButton ;
+
+}
+
+- (OC_GGS_DocumentInterface *) initWithDocument: (OC_GGS_Document *) inDocument ;
+
+- (IBAction) actionGotoLine: (id) inSender ;
+
+- (IBAction) duplicateSelectedSourceViewAction: (id) inSender ;
+
+- (IBAction) actionComment: (id) sender ;
+- (IBAction) actionUncomment: (id) sender ;
+- (IBAction) actionShiftLeft: (id) sender  ;
+- (IBAction) actionShiftRight: (id) sender ;
+
+- (IBAction) actionBuild: (id) sender ;
+- (IBAction) stopBuild: (id) sender ;
+
+- (BOOL) canTerminateApplication ;
+
+- (void) triggerDocumentEditedStatusUpdate ;
+
+- (void) displayIssueDetailedMessage: (NSString *) inDetailledMessage ;
+
+- (NSWindow *) window ;
+
+@end
