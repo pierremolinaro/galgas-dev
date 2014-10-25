d82768eb6fa0f184f98e2177f1c9b8bdf375c4ea
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2014-10-25 11:40:10 +0000

    Suite

--- hand_coded_sources/cVocabulary.cpp
+++ hand_coded_sources/cVocabulary.cpp
@@ -5,10 +5,6 @@
 //  Copyright (C) 1999, ..., 2010 Pierre Molinaro.                                                                     *
 //  e-mail : molinaro@irccyn.ec-nantes.fr                                                                              *
 //  IRCCyN, Institut de Recherche en Communications et Cybernétique de Nantes, ECN, École Centrale de Nantes (France)  *
-//  warranty of MERCHANDIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for            *
-//  more details.                                                                                                      *
-//  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option)  *
-//  any later version.                                                                                                 *
 //                                                                                                                     *
 //  This program is free software; you can redistribute it and/or modify it                                            *
 //  under the terms of the GNU General Public License as published by the                                              *
@@ -17,8 +13,6 @@
 //  This program is distributed in the hope it will be useful, but WITHOUT ANY WARRANTY; without even the implied      *
 //  warranty of MERCHANDIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for            *
 //  more details.                                                                                                      *
-//  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option)  *
-//  any later version.                                                                                                 *
 //                                                                                                                     *
 //---------------------------------------------------------------------------------------------------------------------*
 
