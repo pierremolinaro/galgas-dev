54916b4c39291596c27ab8c6b4bc6b583ecd9efa
Pierre Molinaro <pierre@pcmolinaro.name>
2014-12-17 14:30:42 +0000

    Correction option --mode

--- build/output/all-declarations-12.h
+++ build/output/all-declarations-12.h
@@ -3595,12 +3595,24 @@ class cPtr_programComponentForGeneration : public cPtr_semanticDeclarationWithHe
 
 //---------------------------------------------------------------------------------------------------------------------*
 //                                                                                                                     *
+//                                          Routine 'parseAndAnalyzeProject'                                           *
+//                                                                                                                     *
+//---------------------------------------------------------------------------------------------------------------------*
+
+void routine_parseAndAnalyzeProject (const class GALGAS_lstring constinArgument0,
+                                     const class GALGAS_string constinArgument1,
+                                     class C_Compiler * inCompiler
+                                     COMMA_LOCATION_ARGS) ;
+
+//---------------------------------------------------------------------------------------------------------------------*
+//                                                                                                                     *
 //                                              Routine 'compileProject'                                               *
 //                                                                                                                     *
 //---------------------------------------------------------------------------------------------------------------------*
 
 void routine_compileProject (const class GALGAS_lstring constinArgument0,
                              const class GALGAS_string constinArgument1,
+                             const class GALGAS_galgas_33_ProjectComponentAST constinArgument2,
                              class C_Compiler * inCompiler
                              COMMA_LOCATION_ARGS) ;
 
