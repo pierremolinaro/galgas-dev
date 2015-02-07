2d717dc6d1ff3da6a25c60a2305a4023f1e2e81f
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2015-02-07 21:06:39 +0000

    Suite

---
+++ libpm/python-makefiles/generic_galgas_makefile.py
@@ -0,0 +1,157 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#----------------------------------------------------------------------------------------------------------------------*
+
+import sys, time, os, json
+import makefile, default_build_options
+
+#----------------------------------------------------------------------------------------------------------------------*
+#   displayDurationFromStartTime                                                                                       *
+#----------------------------------------------------------------------------------------------------------------------*
+
+def displayDurationFromStartTime (startTime) :
+  totalDurationInSeconds = int (time.time () - startTime)
+  durationInSecondes = totalDurationInSeconds % 60
+  durationInMinutes = (totalDurationInSeconds / 60) % 60
+  durationInHours = totalDurationInSeconds / 3600
+  s = ""
+  if durationInHours > 0:
+    s += str (durationInHours) + "h"
+  if durationInMinutes > 0:
+    s += str (durationInMinutes) + "min"
+  s += str (durationInSecondes) + "s"
+  print "Done at +" + s
+
+#----------------------------------------------------------------------------------------------------------------------*
+
+class GenericGalgasMakefile :
+  mDictionary = {}
+  mExecutable = ""
+  mGoal = ""
+  mMaxParallelJobs = 0
+  mUseTitles = True
+  mCompilerTool = []
+  mLinkerTool = []
+  mStripTool = []
+  mSudoTool = ""
+  mCompilationMessage = ""
+  mLinkingMessage = ""
+  mInstallationgMessage = ""
+  mAllCompilerOptions = []
+  mCompilerReleaseOptions = []
+  mCompilerDebugOptions = []
+  m_C_CompilerOptions = []
+  m_Cpp_CompilerOptions = []
+  m_ObjectiveC_CompilerOptions = []
+  m_ObjectiveCpp_CompilerOptions = []
+
+  def run (self) :
+    startTime = time.time ()
+  #--- Source file list
+    SOURCES = self.mDictionary ["SOURCES"]
+  #--- LIBPM
+    LIBPM_DIRECTORY_PATH = self.mDictionary ["LIBPM_DIRECTORY_PATH"]
+  #--- Source directory list
+    SOURCES_DIR = self.mDictionary ["SOURCES_DIR"]
+  #--------------------------------------------------------------------------- Include dirs
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/bdd")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/command_line_interface")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/files")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/galgas")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/galgas2")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/streams")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/time")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/strings")
+    SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/utilities")
+    includeDirs = []
+    for d in SOURCES_DIR:
+      includeDirs.append ("-I" + d)
+  #--- Make object
+    make = makefile.Make ()
+  #--------------------------------------------------------------------------- Add Compile rule for sources
+  #--- Object file directory
+    objectDirectory = os.path.normpath (os.getcwd () + "/../build/cli-objects/makefile-macosx-objects")
+  #---
+    objectFileList = []
+    for source in SOURCES:
+      objectFile = objectDirectory + "/" + source + ".o"
+      objectFileList.append (objectFile)
+      sourcePath = make.searchFileInDirectories (source, SOURCES_DIR)
+      if sourcePath != "" :
+        command = []
+        command += self.mCompilerTool
+        command += self.mCompilerReleaseOptions
+        command += self.mAllCompilerOptions
+        command += self.m_Cpp_CompilerOptions
+        command += ["-c", sourcePath]
+        command += ["-o", objectFile]
+        command += includeDirs
+        command += ["-MD", "-MP", "-MF", objectFile + ".dep"]
+        make.addRule (objectFile, [sourcePath], command, self.mCompilationMessage + ": " + source, objectFile + ".dep")
+  #--------------------------------------------------------------------------- Add Compile rule for sources debug
+  #--- Object file directory
+    debugObjectDirectory = os.path.normpath (os.getcwd () + "/../build/cli-objects/makefile-macosx-debug-objects")
+  #---
+    debugObjectFileList = []
+    for source in SOURCES:
+      objectFile = debugObjectDirectory + "/" + source + ".o"
+      debugObjectFileList.append (objectFile)
+      sourcePath = make.searchFileInDirectories (source, SOURCES_DIR)
+      if sourcePath != "" :
+        command = []
+        command += self.mCompilerTool
+        command += self.mCompilerDebugOptions
+        command += self.mAllCompilerOptions
+        command += self.m_Cpp_CompilerOptions
+        command += ["-c", sourcePath]
+        command += ["-o", objectFile]
+        command += includeDirs
+        command += ["-MD", "-MP", "-MF", objectFile + ".dep"]
+        make.addRule (objectFile, [sourcePath], command, self.mCompilationMessage + " - debug: " + source, objectFile + ".dep")
+  #--------------------------------------------------------------------------- Add EXECUTABLE file rule
+    linkCommand = []
+    linkCommand += self.mLinkerTool
+    linkCommand += objectFileList
+    linkCommand += ["-o", self.mExecutable]
+    make.addRule (self.mExecutable, objectFileList, linkCommand, self.mLinkingMessage + ": " + self.mExecutable)
+  #--------------------------------------------------------------------------- Add EXECUTABLE_DEBUG file rule
+    EXECUTABLE_DEBUG = self.mExecutable + "-debug"
+    linkCommand = []
+    linkCommand += self.mLinkerTool
+    linkCommand += debugObjectFileList
+    linkCommand += ["-o", EXECUTABLE_DEBUG]
+    make.addRule (EXECUTABLE_DEBUG, debugObjectFileList, linkCommand, self.mLinkingMessage + " - debug: " + EXECUTABLE_DEBUG)
+  #--------------------------------------------------------------------------- Add install EXECUTABLE file rule
+    INSTALL_EXECUTABLE = "/usr/local/bin/" + self.mExecutable
+    command = []
+    command += self.mSudoTool
+    command += ["cp", self.mExecutable, INSTALL_EXECUTABLE]
+    make.addRule (INSTALL_EXECUTABLE, [self.mExecutable], command, self.mInstallationgMessage + " " + self.mExecutable)
+  #--------------------------------------------------------------------------- Add install EXECUTABLE-debug file rule
+    INSTALL_EXECUTABLE_DEBUG = "/usr/local/bin/" + EXECUTABLE_DEBUG
+    command = []
+    command += self.mSudoTool
+    command += ["cp", EXECUTABLE_DEBUG, INSTALL_EXECUTABLE_DEBUG]
+    make.addRule (INSTALL_EXECUTABLE_DEBUG, [EXECUTABLE_DEBUG], command, self.mInstallationgMessage + " " + EXECUTABLE_DEBUG)
+  #--------------------------------------------------------------------------- Compute jobs
+    if (self.mGoal == "") or (self.mGoal == "all"):
+      make.makeJobs (self.mExecutable)
+      make.makeJobs (EXECUTABLE_DEBUG)
+    elif self.mGoal == "release":
+      make.makeJobs (self.mExecutable)
+    elif self.mGoal == "debug":
+      make.makeJobs (EXECUTABLE_DEBUG)
+    elif self.mGoal == "install-release":
+      make.makeJobs (INSTALL_EXECUTABLE)
+    elif self.mGoal == "install-debug":
+      make.makeJobs (INSTALL_EXECUTABLE_DEBUG)
+    else:
+      make.enterError ("Unknown '" + self.mGoal + "' goal; accepted: '', 'all', 'release', 'debug', 'install-release', 'install-debug'")
+  #--------------------------------------------------------------------------- Run jobs
+    make.runJobs (self.mMaxParallelJobs, self.mUseTitles)
+  #--------------------------------------------------------------------------- Ok ?
+    make.printErrorCountAndExitOnError ()
+    displayDurationFromStartTime (startTime)
+
+#----------------------------------------------------------------------------------------------------------------------*
--- libpm/python-makefiles/macosx_gcc_tools.py
+++ libpm/python-makefiles/macosx_gcc_tools.py
@@ -4,146 +4,41 @@
 #----------------------------------------------------------------------------------------------------------------------*
 
 import sys, time, os, json
-import makefile
-
-#----------------------------------------------------------------------------------------------------------------------*
-#   displayDurationFromStartTime                                                                                       *
-#----------------------------------------------------------------------------------------------------------------------*
-
-def displayDurationFromStartTime (startTime) :
-  totalDurationInSeconds = int (time.time () - startTime)
-  durationInSecondes = totalDurationInSeconds % 60
-  durationInMinutes = (totalDurationInSeconds / 60) % 60
-  durationInHours = totalDurationInSeconds / 3600
-  s = ""
-  if durationInHours > 0:
-    s += str (durationInHours) + "h"
-  if durationInMinutes > 0:
-    s += str (durationInMinutes) + "min"
-  s += str (durationInSecondes) + "s"
-  print "Done at +" + s
+import makefile, default_build_options
+import generic_galgas_makefile
 
 #----------------------------------------------------------------------------------------------------------------------*
 
 def buildForMacOSX (dictionary, EXECUTABLE, GOAL, maxParallelJobs, useTitles) :
-  startTime = time.time ()
-  COMPILER_TOOL = "gcc"
-  LINKER_TOOL   = "g++"
-  STRIP_TOOL    = ["strip", "-A", "-n", "-r", "-u"]
-  SUDO_TOOL     = "sudo"
-  COMPILATION_MESSAGE = "Native Compiling for Mac OS X"
-  LINKING_MESSAGE = "Native Linking for Mac OS X"
+  gmf = generic_galgas_makefile.GenericGalgasMakefile ()
+  gmf.mDictionary = dictionary
+  gmf.mExecutable = EXECUTABLE
+  gmf.mGoal = GOAL
+  gmf.mMaxParallelJobs = maxParallelJobs
+  gmf.mUseTitles = useTitles
+#---
+  gmf.mCompilerTool = ["gcc"]
+  gmf.mLinkerTool   = ["g++"]
+  gmf.mStripTool    = ["strip", "-A", "-n", "-r", "-u"]
+  gmf.mSudoTool     = ["sudo"]
+  gmf.mCompilationMessage = "Native Compiling for Mac OS X"
+  gmf.mLinkingMessage = "Native Linking for Mac OS X"
+  gmf.mInstallationgMessage = "Installing"
 #--- Options for all compilers
-  COMPILER_OPTIONS_FOR_THIS_CHAIN = ["-Wconversion"]
+  gmf.mAllCompilerOptions = default_build_options.allCompilerOptions (["-Wconversion"])
 #--- Options for release mode
-  COMPILER_OPTIONS_RELEASE_FOR_THIS_CHAIN = ["-O2"]
+  gmf.mCompilerReleaseOptions = default_build_options.compilerReleaseOptions (["-O2"])
 #--- Options for debug mode 
-  COMPILER_OPTIONS_DEBUG_FOR_THIS_CHAIN = []
+  gmf.mCompilerDebugOptions = default_build_options.compilerDebugOptions ([])
 #--- Options for C compiling (.c extension)
-  C_COMPILER_OPTIONS_FOR_THIS_CHAIN = []
+  gmf.m_C_CompilerOptions = default_build_options.C_CompilerOptions ([])
 #--- Options for C++ compiling (.cpp extension)
-  CPP_COMPILER_OPTIONS_FOR_THIS_CHAIN = ["-Weffc++", "-Wsign-promo"]
+  gmf.m_Cpp_CompilerOptions = default_build_options.Cpp_CompilerOptions (["-Weffc++", "-Wsign-promo"])
 #--- Options for Objective-C compiling (.m extension)
-  OC_COMPILER_OPTIONS_FOR_THIS_CHAIN = []
+  gmf.m_ObjectiveC_CompilerOptions = default_build_options.ObjectiveC_CompilerOptions ([])
 #--- Options for Objective-C++ compiling (.mm extension)
-  OCPP_COMPILER_OPTIONS_FOR_THIS_CHAIN = []
-#--- Source file list
-  SOURCES = dictionary ["SOURCES"]
-#--- LIBPM
-  LIBPM_DIRECTORY_PATH = dictionary ["LIBPM_DIRECTORY_PATH"]
-#--- Source directory list
-  SOURCES_DIR = dictionary ["SOURCES_DIR"]
-#--------------------------------------------------------------------------- Include dirs
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/bdd")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/command_line_interface")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/files")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/galgas")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/galgas2")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/streams")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/time")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/strings")
-  SOURCES_DIR.append (LIBPM_DIRECTORY_PATH + "/utilities")
-  includeDirs = []
-  for d in SOURCES_DIR:
-    includeDirs.append ("-I" + d)
-#--- Make object
-  make = makefile.Make ()
-#--------------------------------------------------------------------------- Add Compile rule for sources
-#--- Object file directory
-  objectDirectory = os.path.normpath (os.getcwd () + "/../build/cli-objects/makefile-macosx-objects")
-#---
-  objectFileList = []
-  for source in SOURCES:
-    objectFile = objectDirectory + "/" + source + ".o"
-    objectFileList.append (objectFile)
-    sourcePath = make.searchFileInDirectories (source, SOURCES_DIR)
-    if sourcePath != "" :
-      command = [COMPILER_TOOL]
-      command += COMPILER_OPTIONS_FOR_THIS_CHAIN
-      command += COMPILER_OPTIONS_RELEASE_FOR_THIS_CHAIN
-      command += CPP_COMPILER_OPTIONS_FOR_THIS_CHAIN
-      command.append ("-DDO_NOT_GENERATE_CHECKINGS")     
-      command += ["-c", sourcePath]
-      command += ["-o", objectFile]
-      command += includeDirs
-      command += ["-MD", "-MP", "-MF", objectFile + ".dep"]
-      make.addRule (objectFile, [sourcePath], command, COMPILATION_MESSAGE + ": " + source, objectFile + ".dep")
-#--------------------------------------------------------------------------- Add Compile rule for sources debug
-#--- Object file directory
-  debugObjectDirectory = os.path.normpath (os.getcwd () + "/../build/cli-objects/makefile-macosx-debug-objects")
-#---
-  debugObjectFileList = []
-  for source in SOURCES:
-    objectFile = debugObjectDirectory + "/" + source + ".o"
-    debugObjectFileList.append (objectFile)
-    sourcePath = make.searchFileInDirectories (source, SOURCES_DIR)
-    if sourcePath != "" :
-      command = [COMPILER_TOOL]
-      command += COMPILER_OPTIONS_FOR_THIS_CHAIN
-      command += CPP_COMPILER_OPTIONS_FOR_THIS_CHAIN
-      command += ["-c", sourcePath]
-      command += ["-o", objectFile]
-      command += includeDirs
-      command += ["-MD", "-MP", "-MF", objectFile + ".dep"]
-      make.addRule (objectFile, [sourcePath], command, COMPILATION_MESSAGE + " - debug: " + source, objectFile + ".dep")
-#--------------------------------------------------------------------------- Add EXECUTABLE file rule
-  linkCommand = [LINKER_TOOL]
-  linkCommand += objectFileList
-  linkCommand += ["-o", EXECUTABLE]
-  make.addRule (EXECUTABLE, objectFileList, linkCommand, LINKING_MESSAGE + ": " + EXECUTABLE)
-#--------------------------------------------------------------------------- Add EXECUTABLE_DEBUG file rule
-  EXECUTABLE_DEBUG = EXECUTABLE + "-debug"
-  linkCommand = [LINKER_TOOL]
-  linkCommand += debugObjectFileList
-  linkCommand += ["-o", EXECUTABLE_DEBUG]
-  make.addRule (EXECUTABLE_DEBUG, debugObjectFileList, linkCommand, LINKING_MESSAGE + " - debug: " + EXECUTABLE_DEBUG)
-#--------------------------------------------------------------------------- Add install EXECUTABLE file rule
-  INSTALL_EXECUTABLE = "/usr/local/bin/" + EXECUTABLE
-  command = [SUDO_TOOL, "cp", EXECUTABLE, INSTALL_EXECUTABLE]
-  make.addRule (INSTALL_EXECUTABLE, [EXECUTABLE], command, "Installing " + EXECUTABLE)
-#--------------------------------------------------------------------------- Add install EXECUTABLE-debug file rule
-  INSTALL_EXECUTABLE_DEBUG = "/usr/local/bin/" + EXECUTABLE_DEBUG
-  command = [SUDO_TOOL, "cp", EXECUTABLE_DEBUG, INSTALL_EXECUTABLE_DEBUG]
-  make.addRule (INSTALL_EXECUTABLE_DEBUG, [EXECUTABLE_DEBUG], command, "Installing " + EXECUTABLE_DEBUG)
-#--------------------------------------------------------------------------- Compute jobs
-  if (GOAL == "") or (GOAL == "all"):
-    make.makeJobs (EXECUTABLE)
-    make.makeJobs (EXECUTABLE_DEBUG)
-  elif GOAL == "release":
-    make.makeJobs (EXECUTABLE)
-  elif GOAL == "debug":
-    make.makeJobs (EXECUTABLE_DEBUG)
-  elif GOAL == "install-release":
-    make.makeJobs (INSTALL_EXECUTABLE)
-  elif GOAL == "install-debug":
-    make.makeJobs (INSTALL_EXECUTABLE_DEBUG)
-  else:
-    make.enterError ("Unknown '" + GOAL + "' goal; accepted: '', 'all', 'release', 'debug', 'install-release', 'install-debug'")
-#--------------------------------------------------------------------------- Run jobs
-  make.runJobs (maxParallelJobs, useTitles)
-#--------------------------------------------------------------------------- Ok ?
-  make.printErrorCountAndExitOnError ()
-  displayDurationFromStartTime (startTime)
-#----------------------------------------------------------------------------------------------------------------------*
+  gmf.m_ObjectiveCpp_CompilerOptions = default_build_options.ObjectiveCpp_CompilerOptions ([])
+#--- Run makefile
+  gmf.run ()
 
 #----------------------------------------------------------------------------------------------------------------------*
