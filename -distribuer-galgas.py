05889eeb5a890f0dbe8d7b957b98879d83d41f5d
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2015-02-20 10:26:24 +0000

    Suite distribution via script Python

--- -distribuer-galgas.py
+++ -distribuer-galgas.py
@@ -66,12 +66,16 @@ def runHiddenCommand (cmd) :
     str += " " + s
   print BOLD_MAGENTA + str + ENDC
   result = ""
+  compteur = 0
   childProcess = subprocess.Popen (cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   while True:
     line = childProcess.stdout.readline ()
     if line != "":
+      compteur = compteur + 1
       result += line
-      sys.stdout.write (".") # Print without newline
+      if compteur == 10:
+        compteur = 0
+        sys.stdout.write (".") # Print without newline
     else:
       print ""
       childProcess.wait ()
@@ -86,8 +90,6 @@ def runHiddenCommand (cmd) :
 #-------------------------------------- Get script absolute path
 scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
 os.chdir (scriptDir)
-
-
 #-------------------- Supprimer une distribution existante
 TEMP_DIR= os.path.normpath (scriptDir + "/../DISTRIBUTION_GALGAS")
 runCommand (["rm", "-fr", TEMP_DIR])
@@ -113,7 +115,7 @@ print "Version GALGAS : '" + versionGALGAS + "'"
 #--- Renommer le répertoire de construction
 DIR= os.path.normpath (scriptDir + "/../DISTRIBUTION_GALGAS_" + versionGALGAS)
 runCommand (["rm", "-fr", DIR])
-os.rename (TEMP_DIR, DIR)
+runCommand (["mv", TEMP_DIR, DIR])
 #-------------------- Mettre a jour les numéros de version
 writeFile (versionGALGAS, DIR + "/version-galgas.txt")
 remplacerAnneeEtVersionGALGAS (ANNEE, versionGALGAS, DIR + "/galgas/project-xcode-galgas/Info.plist")
@@ -131,30 +133,6 @@ for root, dirs, files in os.walk (DIR + "/galgas/galgas-sources"):
     if not ok:
       ok = extension == ".m"
     remplacerAnneeEtVersionGALGAS (ANNEE, versionGALGAS, root + "/" + filename)
-
-# VERSION_GALGAS=`cat ${TEMP_DIR}/version-galgas.txt` &&
-# DIR=`dirname $0`/../DISTRIBUTION_GALGAS_${VERSION_GALGAS} &&
-# rm -fr ${DIR} &&
-# mv ${TEMP_DIR} ${DIR} &&
-# sed "s/!AN!/${ANNEE}/g" ${DIR}/galgas/project-xcode-galgas/Info.plist > ${DIR}/temp &&
-# sed "s/GALGAS_BETA_VERSION/${VERSION_GALGAS}/g" ${DIR}/temp > ${DIR}/galgas/project-xcode-galgas/Info.plist &&
-# sed "s/!AN!/${ANNEE}/g" ${DIR}/galgas/project-xcode-galgas/English.lproj/InfoPlist.strings > ${DIR}/temp &&
-# sed "s/GALGAS_BETA_VERSION/${VERSION_GALGAS}/g" ${DIR}/temp > ${DIR}/galgas/project-xcode-galgas/English.lproj/InfoPlist.strings &&
-# { 
-#   for fichier in ${DIR}/galgas/galgas-sources/*.galgas \
-#       ${DIR}/galgas/*.galgasProject \
-#       ${DIR}/galgas/build/output/*.h \
-#       ${DIR}/galgas/build/output/*.cpp \
-#       ${DIR}/galgas/build/output/*.m \
-#       ${DIR}/galgas/hand_coded_sources/*.h \
-#       ${DIR}/galgas/hand_coded_sources/*.cpp
-#   do
-#     echo $fichier
-#     sed "s/GALGAS_BETA_VERSION/${VERSION_GALGAS}/g" $fichier > ${DIR}/temp
-#     sed "s/!AN!/${ANNEE}/g" ${DIR}/temp > $fichier
-#   done
-# } &&
-# rm ${DIR}/temp &&
 #-------------------- Creer l'archive des sources de galgas
 runCommand (["eolc", "-unix", "-D" + DIR + "/galgas", "-Eh", "-Ec", "-Ecpp", "-Em", "-Emm", "-Epy", "-Ebat"])
 os.chdir (DIR)
@@ -164,59 +142,19 @@ runCommand (["eolc", "-dos", "-D" + DIR + "/galgas", "-Eh", "-Ec", "-Ecpp", "-Em
 os.chdir (DIR)
 runCommand (["tar", "-cf", "galgas-sources-crlf.tar", "galgas"])
 runCommand (["bzip2", "-9", "galgas-sources-crlf.tar"])
-# cd ${DIR} && tar cv galgas | bzip2 -9 > galgas-sources-lf.tar.bz2 &&
-# eolc -dos -D${DIR}/galgas -Eh -Ec -Ecpp -Em -Emm -Epy -Ebat -Emke -Fmakefile &&
-# cd ${DIR} && tar cv galgas | bzip2 -9 > galgas-sources-crlf.tar.bz2 &&
 #-------------------- Copier changeLog
 runCommand (["mv", DIR + "/galgas/changeLog.html", DIR + "/changeLog.html"])
 #-------------------- Vérifier les programmes d'exemple
 runCommand ([DIR + "/galgas/sample_code/-build-all-macosx.command"])
-# ${DIR}/galgas/sample_code/-build-all-macosx.command &&
-# rm -fr ${DIR}/galgas/sample_code &&
 runCommand (["rm", "-fr", DIR + "/galgas/sample_code"])
 #-------------------- Exécuter les tests
 runCommand ([DIR + "/galgas/testsuite/_run_testsuite.command"])
 runCommand (["rm", "-fr", DIR + "/galgas/testsuite"])
-# ${DIR}/galgas/testsuite/_run_testsuite.command &&
-# rm -fr ${DIR}/galgas/testsuite &&
 #-------------------- Vérifier la création de projet
 runCommand ([DIR + "/galgas/-verifier-create-galgas.command"])
-# GALGAS_DEBUG_TOOL=${DIR}/galgas/makefile-macosx/galgas-debug &&
-# TEST_PROJECT_NAME=TEST &&
-# CREATE_PROJECT_TEST_DIR=${DIR}/galgas/${TEST_PROJECT_NAME} &&
-# echo "---------------- CREATE PROJECT --------------------------" &&
-# cd ${DIR} && ${GALGAS_DEBUG_TOOL} --create-project=${CREATE_PROJECT_TEST_DIR} &&
-# echo "---------------- GALGAS COMPILE --------------------------" &&
-# ${GALGAS_DEBUG_TOOL} -v --Werror ${CREATE_PROJECT_TEST_DIR}/+${TEST_PROJECT_NAME}.galgasProject &&
-# echo "---------------- XCODE BUILD --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/xcode-project && xcodebuild -alltargets &&
-# echo "---------------- MAKE i386 Linux --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-x86linux32-on-macosx && python ./build.py &&
-# echo "---------------- MAKE x86_64 Linux --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-x86linux64-on-macosx && python ./build.py &&
-# echo "---------------- MAKE Unix --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-unix && python ./build.py &&
-# echo "---------------- MAKE win32 --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-win32-on-macosx && python ./build.py &&
-# echo "---------------- MAKE --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-macosx && python ./build.py &&
-# echo "---------------- EXECUTE NORMAL --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-macosx && ./${TEST_PROJECT_NAME} --help &&
-# echo "---------------- EXECUTE DEBUG --------------------------" &&
-# cd ${CREATE_PROJECT_TEST_DIR}/makefile-macosx && ./${TEST_PROJECT_NAME}-debug --help &&
-# echo "---------------- REMOVE PROJECT --------------------------" &&
-# cd ${DIR} &&
-# rm -fr ${CREATE_PROJECT_TEST_DIR} &&
-# echo "---------------- SUCCES --------------------------"
 #-------------------- Construire la documentation Latex
 remplacerAnneeEtVersionGALGAS (ANNEE, versionGALGAS, DIR + "/galgas/galgas-documentation-latex-sources/galgas-book.tex")
-# sed "s/GALGASCURRENTVERSION/${VERSION_GALGAS}/g" ${DIR}/galgas/galgas-documentation-latex-sources/galgas-book.tex > ${DIR}/temp &&
-# cp ${DIR}/temp ${DIR}/galgas/galgas-documentation-latex-sources/galgas-book.tex &&
-# rm ${DIR}/temp &&
-# ${DIR}/galgas/galgas-documentation-latex-sources/-build.command &&
-# cd ${DIR} && cp galgas/galgas-documentation-latex-sources/galgas-book.pdf galgas-book.pdf &&
-# rm -fr ${DIR}/galgas/galgas-documentation-latex-sources &&
-runCommand ([DIR + "/galgas/galgas-documentation-latex-sources/-build.command"])
+runHiddenCommand ([DIR + "/galgas/galgas-documentation-latex-sources/-build.command"])
 runCommand (["cp", "galgas/galgas-documentation-latex-sources/galgas-book.pdf", "galgas-book.pdf"])
 runCommand (["rm", "-fr", "galgas/galgas-documentation-latex-sources"])
 #-------------------- Creer l'archive de l'executable windows (release et debug)
@@ -225,75 +163,46 @@ runCommand (["python", "build.py"])
 runCommand (["bzip2", "-9", "galgas.exe"])
 runCommand (["bzip2", "-9", "galgas-debug.exe"])
 os.chdir (DIR)
-runCommand (["cp", DIR + "/galgas/makefile-win32-on-macosx/galgas.exe", "galgas.exe"])
-runCommand (["cp", DIR + "/galgas/makefile-win32-on-macosx/galgas-debug.exe", "galgas-debug.exe"])
+runCommand (["mv", DIR + "/galgas/makefile-win32-on-macosx/galgas.exe.bz2", "galgas.exe.bz2"])
+runCommand (["mv", DIR + "/galgas/makefile-win32-on-macosx/galgas-debug.exe.bz2", "galgas-debug.exe.bz2"])
 runCommand (["rm", "-fr", "galgas/makefile-win32-on-macosx"])
 runCommand (["rm", "-fr", "galgas/build/cli-objects"])
-# cd ${DIR}/galgas/makefile-win32-on-macosx/ && python ./build.py &&
-# cp ${DIR}/galgas/makefile-win32-on-macosx/galgas.exe ${DIR}/galgas.exe &&
-# cd ${DIR} && bzip2 -9 galgas.exe &&
-# cp ${DIR}/galgas/makefile-win32-on-macosx/galgas-debug.exe ${DIR}/galgas-debug.exe &&
-# cd ${DIR} && bzip2 -9 galgas-debug.exe &&
-# rm -fr ${DIR}/galgas/makefile-win32-on-macosx &&
-# rm -fr ${DIR}/galgas/build/cli-objects &&
 #-------------------- Creer l'archive de l'executable x86 linux 32 (release et debug)
 os.chdir (DIR + "/galgas/makefile-x86linux32-on-macosx")
 runCommand (["python", "build.py"])
-runCommand (["bzip2", "-9", "galgas.exe"])
-runCommand (["bzip2", "-9", "galgas-debug.exe"])
+runCommand (["zip", "-9", "galgas.zip", "galgas"])
+runCommand (["zip", "-9", "galgas-debug.zip", "galgas-debug"])
 os.chdir (DIR)
-runCommand (["cp", DIR + "/galgas/makefile-x86linux32-on-macosx/galgas.exe", "galgas.exe"])
-runCommand (["cp", DIR + "/galgas/makefile-x86linux32-on-macosx/galgas-debug.exe", "galgas-debug.exe"])
+runCommand (["mv", DIR + "/galgas/makefile-x86linux32-on-macosx/galgas.zip", "galgas-x86-linux32.zip"])
+runCommand (["mv", DIR + "/galgas/makefile-x86linux32-on-macosx/galgas-debug.zip", "galgas-debug-x86-linux32.zip"])
 runCommand (["rm", "-fr", "galgas/makefile-x86linux32-on-macosx"])
 runCommand (["rm", "-fr", "galgas/build/cli-objects"])
-# cd ${DIR}/galgas/makefile-x86linux32-on-macosx/ && python ./build.py &&
-# cd ${DIR}/galgas/makefile-x86linux32-on-macosx && zip -9 galgas.zip galgas &&
-# cp ${DIR}/galgas/makefile-x86linux32-on-macosx/galgas.zip ${DIR}/galgas-x86-linux32.zip &&
-# cd ${DIR}/galgas/makefile-x86linux32-on-macosx && zip -9 galgas-debug.zip galgas-debug &&
-# cp ${DIR}/galgas/makefile-x86linux32-on-macosx/galgas-debug.zip ${DIR}/galgas-debug-x86-linux32.zip &&
-# rm -fr ${DIR}/galgas/makefile-x86linux32-on-macosx &&
-# rm -fr ${DIR}/galgas/build/cli-objects &&
 #-------------------- Creer l'archive de l'executable x86 linux 64 (release et debug)
 os.chdir (DIR + "/galgas/makefile-x86linux64-on-macosx")
 runCommand (["python", "build.py"])
-runCommand (["bzip2", "-9", "galgas.exe"])
-runCommand (["bzip2", "-9", "galgas-debug.exe"])
+runCommand (["zip", "-9", "galgas.zip", "galgas"])
+runCommand (["zip", "-9", "galgas-debug.zip", "galgas-debug"])
 os.chdir (DIR)
-runCommand (["cp", DIR + "/galgas/makefile-x86linux64-on-macosx/galgas.exe", "galgas.exe"])
-runCommand (["cp", DIR + "/galgas/makefile-x86linux64-on-macosx/galgas-debug.exe", "galgas-debug.exe"])
+runCommand (["mv", DIR + "/galgas/makefile-x86linux64-on-macosx/galgas.zip", "galgas-x86-linux64.zip"])
+runCommand (["mv", DIR + "/galgas/makefile-x86linux64-on-macosx/galgas-debug.zip", "galgas-debug-x86-linux64.zip"])
 runCommand (["rm", "-fr", "galgas/makefile-x86linux64-on-macosx"])
 runCommand (["rm", "-fr", "galgas/build/cli-objects"])
-# cd ${DIR}/galgas/makefile-x86linux64-on-macosx/ && python ./build.py &&
-# cd ${DIR}/galgas/makefile-x86linux64-on-macosx && zip -9 galgas.zip galgas &&
-# cp ${DIR}/galgas/makefile-x86linux64-on-macosx/galgas.zip ${DIR}/galgas-x86-linux64.zip &&
-# cd ${DIR}/galgas/makefile-x86linux64-on-macosx && zip -9 galgas-debug.zip galgas-debug &&
-# cp ${DIR}/galgas/makefile-x86linux64-on-macosx/galgas-debug.zip ${DIR}/galgas-debug-x86-linux64.zip &&
-# rm -fr ${DIR}/galgas/makefile-x86linux64-on-macosx &&
-# rm -fr ${DIR}/galgas/build/cli-objects &&
 #-------------------- Recompiler le projet Xcode
-os.chdir (DIR + "/galgas/mproject-xcode-galgas")
+os.chdir (DIR + "/galgas/project-xcode-galgas")
 runCommand (["xcodebuild", "-project", "galgas-distribution.xcodeproj", "-target", "GALGAS Cocoa", "-configuration", "Default"])
 os.chdir (DIR)
-# cd ${DIR}/galgas/project-xcode-galgas && xcodebuild -project galgas-distribution.xcodeproj -target "GALGAS Cocoa" -configuration Default &&
 #-------------------- Creer l'archive BZ2 de cocoa galgas
 runCommand (["cp", "-r", DIR + "/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app", DIR])
 runCommand (["tar", "-cf", "cocoaGalgas.app.tar", "cocoaGalgas.app"])
 runCommand (["bzip2", "-9", "cocoaGalgas.app.tar"])
-# cp -r ${DIR}/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app ${DIR} &&
-# cd ${DIR} && tar cv cocoaGalgas.app | bzip2 -9 > cocoaGalgas.app.tar.bz2 &&
-# rm -r ${DIR}/cocoaGalgas.app &&
+runCommand (["rm", "-fr", DIR + "/cocoaGalgas.app"])
 #-------------------- Creer l'archive de Cocoa Galgas
 runCommand (["mkdir", DIR + "/COCOA-GALGAS"])
 runCommand (["cp", DIR + "/galgas/AUTHORS", DIR + "/COCOA-GALGAS"])
 runCommand (["cp", DIR + "/galgas/COPYING", DIR + "/COCOA-GALGAS"])
 runCommand (["cp", "-r", DIR + "/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app", DIR + "/COCOA-GALGAS"])
 runCommand (["hdiutil", "create", "-srcfolder", DIR + "/COCOA-GALGAS", DIR + "/cocoa-galgas.dmg"])
-# mkdir ${DIR}/COCOA-GALGAS &&
-# cp ${DIR}/galgas/AUTHORS ${DIR}/COCOA-GALGAS &&
-# cp ${DIR}/galgas/COPYING ${DIR}/COCOA-GALGAS &&
-# cp -r ${DIR}/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app ${DIR}/COCOA-GALGAS &&
-# hdiutil create -srcfolder ${DIR}/COCOA-GALGAS ${DIR}/cocoa-galgas.dmg &&
-# rm -fr ${DIR}/COCOA-GALGAS &&
+runCommand (["rm", "-fr", DIR + "/COCOA-GALGAS"])
 #-------------------- Creer l'archive de l'outil ligne de commande pour mac
 runCommand (["mkdir", DIR + "/COCOA-TOOL"])
 runCommand (["cp", DIR + "/galgas/AUTHORS", DIR + "/COCOA-TOOL"])
@@ -301,19 +210,9 @@ runCommand (["cp", DIR + "/galgas/COPYING", DIR + "/COCOA-TOOL"])
 runCommand (["cp", "-r", DIR + "/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app/Contents/Resources/galgas", DIR + "/COCOA-TOOL/galgas"])
 runCommand (["cp", "-r", DIR + "/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app/Contents/Resources/galgas-debug", DIR + "/COCOA-TOOL/galgas-debug"])
 runCommand (["hdiutil", "create", "-srcfolder", DIR + "/COCOA-TOOL", DIR + "/cocoa-tool.dmg"])
-# mkdir ${DIR}/GALGAS-TOOL &&
-# cp ${DIR}/galgas/AUTHORS ${DIR}/GALGAS-TOOL &&
-# cp ${DIR}/galgas/COPYING ${DIR}/GALGAS-TOOL &&
-# cp ${DIR}/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app/Contents/Resources/galgas ${DIR}/GALGAS-TOOL/galgas &&
-# cp ${DIR}/galgas/project-xcode-galgas/build/Default/cocoaGalgas.app/Contents/Resources/galgas ${DIR}/GALGAS-TOOL/galgasDebug &&
-# hdiutil create -srcfolder ${DIR}/GALGAS-TOOL ${DIR}/galgas-tool.dmg &&
-# rm -fr ${DIR}/GALGAS-TOOL &&
+runCommand (["rm", "-fr", DIR + "/COCOA-TOOL"])
 #-------------------- Supprimer les repertoires sources
-runCommand (["cp", "-fr", DIR + "/galgas"])
+runCommand (["rm", "-fr", DIR + "/galgas"])
 print BOLD_GREEN + "-------------- SUCCES ---------------" + ENDC
-# cd ${DIR} &&
-# rm -fr ${DIR}/galgas &&
-# echo "-------------- SUCCES ---------------" ||
-# echo "-------------- ECHEC ----------------"
 
 #------------------------------------------------------------------------------*
