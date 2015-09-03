75629c316073fd62f0b99068555b5531d4185ab5
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2015-09-03 17:48:08 +0000

    Suite GMP

--- libpm/gmp/Darwin-i386/include/gmp.h
+++ libpm/gmp/Darwin-i386/include/gmp.h
@@ -2289,7 +2289,7 @@ enum
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
 #define __GMP_CC "gcc"
-#define __GMP_CFLAGS "-O2 -pedantic -fomit-frame-pointer -m64 -mtune=corei7 -march=corei7"
+#define __GMP_CFLAGS "-O2 -pedantic -fomit-frame-pointer -m64"
 
 /* Major version number is the value of __GNU_MP__ too, above and in mp.h. */
 #define __GNU_MP_VERSION            6
