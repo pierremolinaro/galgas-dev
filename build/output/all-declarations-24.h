8f16a8fb5ebafd725ef1567aa0307e95880123b3
pm <pm@91611271-8fed-0310-a325-b14c2f6e9f62>
2016-09-07 18:57:21 +0000

    Correction GALGAS_string

--- build/output/all-declarations-24.h
+++ build/output/all-declarations-24.h
@@ -1213,6 +1213,5 @@ extern const cDirectoryWrapper gWrapperDirectory_12_libpmFileWrapper ;
 extern const cDirectoryWrapper gWrapperDirectory_13_libpmFileWrapper ;
 extern const cDirectoryWrapper gWrapperDirectory_14_libpmFileWrapper ;
 extern const cDirectoryWrapper gWrapperDirectory_15_libpmFileWrapper ;
-extern const cDirectoryWrapper gWrapperDirectory_16_libpmFileWrapper ;
 
 #endif
