b2a7419d058960f0a5bb3e32ad866af1f4872922
Pierre Molinaro <pierre@pcmolinaro.name>
2016-10-07 07:31:44 +0000

    Correction TC_Array ; pb insulate sur opérateur ()

--- libpm/utilities/MF_MemoryControl.h
+++ libpm/utilities/MF_MemoryControl.h
@@ -9,7 +9,7 @@
 //  e-mail : pierre.molinaro@irccyn.ec-nantes.fr                                                                       *
 //                                                                                                                     *
 //  IRCCyN, Institut de Recherche en Communications et Cybernétique de Nantes                                          *
-//  ECN, …cole Centrale de Nantes (France)                                                                             *
+//  ECN, École Centrale de Nantes (France)                                                                             *
 //                                                                                                                     *
 //  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General  *
 //  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option)  *
