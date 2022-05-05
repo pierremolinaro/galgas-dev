537130b116cd8b6c7746b82975abe481030707c6
pierremolinaro <pierre@pcmolinaro.name>
2022-05-05 13:49:28 +0200

    Ajout test suite

---
+++ testsuite/+run-test-suite.command
@@ -0,0 +1,20 @@
+#!/bin/sh
+#set -x
+GALGAS_TOOL=`dirname $0`/../makefile-macosx/galgas-debug &&
+cd `dirname $0`/../makefile-macosx && python build.py debug &&
+cd `dirname $0` && $GALGAS_TOOL --generate-many-cpp-files --Werror +testsuite.galgasProject &&
+cd `dirname $0`/makefile-macosx && python build.py &&
+echo "*** Running test suite" &&
+cd `dirname $0` && ./makefile-macosx/testsuite > results.txt &&
+if [ "`cat results.txt`" != "`cat results_reference.txt`" ]; then
+  /Applications/Xcode.app/Contents/Developer/usr/bin/opendiff results.txt results_reference.txt
+  echo "*************************"
+  echo "*        FAILURE        *"
+  echo "*************************"
+else
+  echo "*** Running test suite (debug mode)" &&
+  cd `dirname $0` && ./makefile-macosx/testsuite-debug -q &&
+  echo "*************************"
+  echo "*        SUCCESS        *"
+  echo "*************************"
+fi || ( echo "*************************" ; echo "*     RUN-TIME-ERROR    *" ; echo "*************************" ; exit 1 )
---
+++ testsuite/hand_coded_sources/extern_type_implementation.cpp
@@ -0,0 +1,96 @@
+
+#include "all-declarations.h"
+
+//---------------------------------------------------------------------------*
+
+GALGAS_complex::GALGAS_complex (void) :
+AC_GALGAS_root (),
+mIsValid (false),
+mReal (0.0),
+mImaginary (0.0) {
+}
+
+
+//---------------------------------------------------------------------------*
+
+bool GALGAS_complex::isValid (void) const {
+  return mIsValid ;
+}
+
+//---------------------------------------------------------------------------*
+
+void GALGAS_complex::drop (void) {
+  mIsValid = false ;
+}
+
+//---------------------------------------------------------------------------*
+
+void GALGAS_complex::description (C_String & ioString,
+                                  const int32_t /* inIndentation */) const {
+  ioString << "<@complex:" ;
+  if (mIsValid) {
+    ioString << cStringWithDouble (mReal) << ", " << cStringWithDouble (mImaginary) ;
+  }else{
+    ioString << "invalid" ;
+  }
+  ioString << ">" ;
+}
+
+//---------------------------------------------------------------------------*
+
+typeComparisonResult GALGAS_complex::objectCompare (const GALGAS_complex & inOperand) const {
+  typeComparisonResult result = kOperandNotValid ;
+  if (isValid () && inOperand.isValid ()) {
+    if (mReal < inOperand.mReal) {
+      result = kFirstOperandLowerThanSecond ;
+    }else if (mReal > inOperand.mReal) {
+      result = kFirstOperandGreaterThanSecond ;
+    }else if (mImaginary < inOperand.mImaginary) {
+      result = kFirstOperandLowerThanSecond ;
+    }else if (mImaginary > inOperand.mImaginary) {
+      result = kFirstOperandGreaterThanSecond ;
+    }else{
+      result = kOperandEqual ;
+    }  
+  }
+  return result ;
+}
+
+//---------------------------------------------------------------------------*
+
+GALGAS_complex GALGAS_complex::constructor_new (const GALGAS_double & inReal,
+                                                const GALGAS_double & inImaginary,
+                                                C_Compiler * /* inCompiler */
+                                                COMMA_UNUSED_LOCATION_ARGS) {
+  GALGAS_complex result ;
+  if (inReal.isValid () && inImaginary.isValid ()) {
+    result.mIsValid = true ;
+    result.mReal = inReal.doubleValue () ;
+    result.mImaginary = inImaginary.doubleValue () ;
+  }
+  return result ;
+}
+
+//---------------------------------------------------------------------------*
+
+GALGAS_double GALGAS_complex::getter_real (C_Compiler * /* inCompiler */
+                                           COMMA_UNUSED_LOCATION_ARGS) const {
+  GALGAS_double result ;
+  if (isValid ()) {
+    result = GALGAS_double (mReal) ;
+  }
+  return result ;
+}
+
+//---------------------------------------------------------------------------*
+
+GALGAS_double GALGAS_complex::getter_imaginary (C_Compiler * /* inCompiler */
+                                                COMMA_UNUSED_LOCATION_ARGS) const {
+  GALGAS_double result ;
+  if (isValid ()) {
+    result = GALGAS_double (mImaginary) ;
+  }
+  return result ;
+}
+
+//---------------------------------------------------------------------------*
