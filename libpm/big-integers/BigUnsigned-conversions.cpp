2d321a4187c825f33427bb62d39a2833c84209fb
pierremolinaro <pierre@pcmolinaro.name>
2023-12-16 13:19:40 +0100

    Correction pour CodeQL

--- libpm/big-integers/BigUnsigned-conversions.cpp
+++ libpm/big-integers/BigUnsigned-conversions.cpp
@@ -20,12 +20,12 @@ uint32_t BigUnsigned::requiredBitCountForUnsignedRepresentation (void) const {
   if (isZero ()) {
     return 0 ;
   }else{
-    uint32_t n = uint32_t (u8Count ()) * 8 ;
+    uint32_t n = uint32_t (u8Count () - 1) * 8 ;
     uint8_t last = u8AtIndex (u8Count () - 1) ;
     MF_Assert (last != 0, "last is null", 0, 0) ;
-    while ((last & 0x80) == 0) {
-      n -= 1 ;
-      last <<= 1 ;
+    while (last != 0) {
+      n += 1 ;
+      last /= 2 ;
     }
     return n ;
   }
